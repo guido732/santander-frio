9582cefd1c174a77b328b31382c8cff4ff703346
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-02-23 11:45:45 -0300

    Deleting console.logs in login fetch

--- loginFetch.js
+++ loginFetch.js
@@ -4,8 +4,6 @@ const doLogin = async () => {
   const userDni = document.querySelector("#userdni").value;
   const userPassword = document.querySelector("#password").value;
   const requestBody = { dni: userDni, password: userPassword };
-  console.log(JSON.stringify(requestBody));
-
   response = await fetch("http://127.0.0.1:3000/v1/users/login", {
     method: "POST",
     body: JSON.stringify(requestBody),
@@ -14,5 +12,4 @@ const doLogin = async () => {
     }
   });
   jsonResponse = await response.json();
-  console.log(jsonResponse);
 };
