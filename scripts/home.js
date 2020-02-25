f53262ba3fa01d5ec2ba0930d2d92319c06795d5
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-02-25 17:03:03 -0300

    Rollback to LiveServer. Adding folders. Fixing paths

--- scripts/home.js
+++ scripts/home.js
@@ -1,46 +1,43 @@
 const renderData = () => {
-  let activeUser = JSON.parse(sessionStorage.getItem("activeUser"));
-  let name = activeUser.fullname;
-  let arsBalance = activeUser.accounts[0].balance;
-  document.getElementById("name").innerHTML = name;
-  document.getElementById("walletArs").innerHTML = arsBalance;
-  // Falta hacer que el active user muestre la cuenta en dolares
+	let activeUser = JSON.parse(sessionStorage.getItem("activeUser"));
+	let name = activeUser.fullname;
+	let arsBalance = activeUser.accounts[0].balance;
+	document.getElementById("name").innerHTML = name;
+	document.getElementById("walletArs").innerHTML = arsBalance;
+	// Falta hacer que el active user muestre la cuenta en dolares
 };
 
 // Ingresar dinero
 const addMoney = async e => {
-  e.preventDefault();
-  let activeUser = JSON.parse(sessionStorage.getItem("activeUser"));
-  const accountNum = findAccountNumber(activeUser);
-  const { dni } = activeUser;
-  let qty = {
-    amount: document.getElementById("qtyDeposit").value,
-    userID: dni,
-    destinationAccountNum: accountNum
-  };
-  response = await fetch(
-    "http://127.0.0.1:3000/v1/accounts/operations/depositMoney",
-    {
-      method: "PUT",
-      body: JSON.stringify(qty),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-  );
-  const jsonResponse = await response.json();
+	e.preventDefault();
+	let activeUser = JSON.parse(sessionStorage.getItem("activeUser"));
+	const accountNum = findAccountNumber(activeUser);
+	const { dni } = activeUser;
+	let qty = {
+		amount: document.getElementById("qtyDeposit").value,
+		userID: dni,
+		destinationAccountNum: accountNum,
+	};
+	response = await fetch("http://127.0.0.1:3000/v1/accounts/operations/depositMoney", {
+		method: "PUT",
+		body: JSON.stringify(qty),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	const jsonResponse = await response.json();
 
-  if (response.status === 200) {
-    sessionStorage.setItem("activeUser", JSON.stringify(jsonResponse));
-    renderData();
-  } else {
-    console.log(jsonResponse); /// TODO: Aca hay que mostrar un modal en el front diciendo que alguno de los datos son incorrectos.
-  }
+	if (response.status === 200) {
+		sessionStorage.setItem("activeUser", JSON.stringify(jsonResponse));
+		renderData();
+	} else {
+		console.log(jsonResponse); /// TODO: Aca hay que mostrar un modal en el front diciendo que alguno de los datos son incorrectos.
+	}
 };
 
 const findAccountNumber = activeUser => {
-  const foundAccount = activeUser.accounts.find(acc => acc.currency === "$");
-  return foundAccount.accountNumber;
+	const foundAccount = activeUser.accounts.find(acc => acc.currency === "$");
+	return foundAccount.accountNumber;
 };
 
 document.getElementById("btn-internTransf").addEventListener("click", addMoney);
--- scripts/logoutFetch.js
+++ scripts/logoutFetch.js
@@ -1,25 +1,25 @@
 const doLogout = async () => {
-  try {
-    let activeUser = JSON.parse(sessionStorage.getItem("activeUser"));
-    let userDni = activeUser.dni;
-    const requestBody = { dni: userDni };
-    response = await fetch("http://127.0.0.1:3000/v1/users/logout", {
-      method: "POST",
-      body: JSON.stringify(requestBody),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-    const jsonResponse = await response.json();
-    if (response.status === 200) {
-      sessionStorage.setItem("activeUser", JSON.stringify(jsonResponse));
-      window.location.assign("http://127.0.0.1:5500/");
-    } else {
-      console.log(jsonResponse); /// TODO: Aca hay que mostrar un modal en el front diciendo que alguno de los datos son incorrectos.
-    }
-  } catch (error) {
-    console.log(error); //MANEJO DE ERRORES
-  }
+	try {
+		let activeUser = JSON.parse(sessionStorage.getItem("activeUser"));
+		let userDni = activeUser.dni;
+		const requestBody = { dni: userDni };
+		response = await fetch("http://127.0.0.1:3000/v1/users/logout", {
+			method: "POST",
+			body: JSON.stringify(requestBody),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const jsonResponse = await response.json();
+		if (response.status === 200) {
+			sessionStorage.setItem("activeUser", JSON.stringify(jsonResponse));
+			window.location.assign("/");
+		} else {
+			console.log(jsonResponse); /// TODO: Aca hay que mostrar un modal en el front diciendo que alguno de los datos son incorrectos.
+		}
+	} catch (error) {
+		console.log(error); //MANEJO DE ERRORES
+	}
 };
 
 document.getElementById("logoutAction").addEventListener("click", doLogout);
